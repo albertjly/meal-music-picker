72fedf7fb838f86237947affed500bb913d717a8
MHirano <michelle@hiranohomes.com>
2021-03-25 13:53:49 -0700

    formatting

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,23 +20,24 @@ fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=chicken" , {
 
 // variable for music chosen
 window.onload=function(){
-var chosenMusic = fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=chicken" , {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "745e72bfb2mshcd1b1af9ded37c3p1ca71djsnfeb89c9db301",
-		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
+    var chosenMusic = fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=chicken" , {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "745e72bfb2mshcd1b1af9ded37c3p1ca71djsnfeb89c9db301",
+            "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
+        }
+    })
+    .then(response => {
+        console.log(response);
+    })
+    .catch(err => {
+        console.error(err);
+    });
 
-//append to html
-var musicPlay = document.getElementById('audio');
-musicPlay.src = 'https://cdns-preview-b.dzcdn.net/stream/c-b805a55caedd124afe51f51315428e09-3.mp3';
-Audio.load();
-Audio.play();
-}
+    //append to html
+    var musicPlay = document.getElementById('audio');
+    musicPlay.src = 'https://cdns-preview-b.dzcdn.net/stream/c-b805a55caedd124afe51f51315428e09-3.mp3';
+    
+    Audio.load();
+    Audio.play();
+    }
