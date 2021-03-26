b9767a2081d34278db3d92a3644442513280494e
MHirano <michelle@hiranohomes.com>
2021-03-25 20:40:34 -0700

    .then to json

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,39 +1,44 @@
 var protien = document.querySelector("#protein-search");
 
 
+
+
+
+
+
+
 //fetch music
-fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=" + protein, {
+fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=chicken", {
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-key": "745e72bfb2mshcd1b1af9ded37c3p1ca71djsnfeb89c9db301",
 		"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
 	}
 })
-.then(response => {
-	console.log(response);
-})
+.then(response => response.json())
+.then(data => console.log(data))
 .catch(err => {
 	console.error(err);
 });
 
 // variable for music chosen
 window.onload=function(){    
-    var chosenMusic = fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=" + protein , {
+    var chosenMusic = fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=chicken" , {
         "method": "GET",
         "headers": {
             "x-rapidapi-key": "745e72bfb2mshcd1b1af9ded37c3p1ca71djsnfeb89c9db301",
             "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
         }
     })
-    .then(response => {
-        console.log(response);
-    })
+    .then(response => response.json())
+    .then(data => console.log(data))
     .catch(err => {
         console.error(err);
     });
 
+    
     //append to html
     var musicPlay = document.getElementById('audio');
-    musicPlay.src = chosenMusic;
+    musicPlay.src = 'https://cdns-preview-b.dzcdn.net/stream/c-b805a55caedd124afe51f51315428e09-3.mp3' ;
     
     }
