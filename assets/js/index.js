8abd42d459bba185243534cfecd0a0913152a6ea
MHirano <michelle@hiranohomes.com>
2021-03-26 21:49:01 -0700

    add music on load

--- assets/js/index.js
+++ assets/js/index.js
@@ -206,6 +206,28 @@ var displayRecipe = function (title, img, yield, ingridents, dietLabels, nutriti
 
 }
 
+// variable for music chosen
+window.onload=function(){  
+
+    fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=" + protein , {
+         "method": "GET",
+         "headers": {
+             "x-rapidapi-key": "745e72bfb2mshcd1b1af9ded37c3p1ca71djsnfeb89c9db301",
+             "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
+         }
+     })
+     .then(response => response.json())
+     .then((response) => {
+         let chosenMusic = response.data[2].preview;
+         //append to html
+     var musicPlay = document.getElementById('audio');
+     musicPlay.src = chosenMusic ;
+ 
+     })
+     
+     }
+ 
+
 var clear = function () {
     mealIngredientsCon.innerHTML = ""; 
     mealYieldCon.innerHTML = ""; 
