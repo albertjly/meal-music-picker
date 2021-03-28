7395c3fe2d03e9d0f3d08752f8a53a76c3d8e97c
MHirano <michelle@hiranohomes.com>
2021-03-28 11:53:55 -0700

    remove commas

--- assets/js/box.js
+++ assets/js/box.js
@@ -5,8 +5,19 @@ recipeSave = function(){
     var getStore =  JSON.parse(localStorage.getItem('title')) || [] ;
     let localStorageItems = Object.keys(localStorage)
     arr.push(localStorageItems)
-    console.log(arr)
-    recipeCon.innerHTML=`<ul>${arr[0].map(i=>`<li>${i}</li>`)}</ul>`
+   let ul = document.createElement("ul")
+   for (let i = 0; i < arr[0].length; i++) {
+        let li = document.createElement("li");
+        li.textContent = arr[0][i]
+        ul.appendChild(li);
+        
+   }
+   recipeCon.appendChild(ul); 
+    //recipeCon.innerHTML=`<ul>${arr[0].map(i=>`<li>${i}</li>`)}</ul>`
+   // localStorageItems.join("");
+    //arr[0].map(i=>{
+        //console.log(i);
+   // })
     // localStorage.getItem('getStore', JSON.stringify(getStore));
     // console.log(storedRecipe);
     
