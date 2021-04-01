2b5146cedfaebbae91e8d666a31bdf19ac740451
Albert Jiang <albertjly@gmail.com>
2021-03-31 21:47:58 -0700

    add favicon, fix box css, adjust a few css problem

--- assets/js/box.js
+++ assets/js/box.js
@@ -1,27 +1,22 @@
+//Dom Element
 var recipeCon = document.querySelector(".recipes-box");
 
+
+
+//Function to save recipes and append to page
 recipeSave = function(){
     let arr= [];
     var getStore =  JSON.parse(localStorage.getItem('title')) || [] ;
     let localStorageItems = Object.keys(localStorage);
     arr.push(localStorageItems);
-   let ul = document.createElement("ul");
-   for (let i = 0; i < arr[0].length; i++) {
+    let ul = document.createElement("ul");
+    for (let i = 0; i < arr[0].length; i++) {
         let li = document.createElement("li");
+        li.setAttribute("class", "column is-12 p-1 m-2 fas fa-drumstick-bite");
+        li.setAttribute("style", "color: #D2692E" );
         li.textContent = arr[0][i];
         recipeCon.appendChild(li);
-        
-   }
-   
-    //recipeCon.innerHTML=`<ul>${arr[0].map(i=>`<li>${i}</li>`)}</ul>`
-   // localStorageItems.join("");
-    //arr[0].map(i=>{
-        //console.log(i);
-   // })
-    // localStorage.getItem('getStore', JSON.stringify(getStore));
-    // console.log(storedRecipe);
-    
- };
- 
- recipeSave();
+    }
+};
 
+recipeSave();
